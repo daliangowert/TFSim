18c2f82dead3301d3a5845100a96f97e915bf394
Joao-Schio <jp@santoandrea.com.br>
2023-06-30 10:49:01 -0400

    funcao de calcular fatorial adicionada, caso teste sendo 7

--- src/main.cpp
+++ src/main.cpp
@@ -686,6 +686,54 @@ int sc_main(int argc, char *argv[])
                 inFile.close();
             }
     });
+    bench_sub -> append("Fatorial",[&](menu::item_proxy &ip){
+        string path = "in/benchmarks/fatorial/code.txt";
+        bench_name = "Fatorial";
+        inFile.open(path);
+        if(!add_instructions(inFile,instruction_queue,instruct))
+            show_message("Arquivo inválido","Não foi possível abrir o arquivo");
+        else
+            fila = true;
+        path = "in/benchmarks/fatorial/regs.txt";
+        inFile.open(path);
+            if(!inFile.is_open())
+                show_message("Arquivo inválido","Não foi possível abrir o arquivo!");
+            else
+            {
+                auto reg_gui = reg.at(0);
+                int value,i = 0;
+                while(inFile >> value && i < 32)
+                {
+                    reg_gui.at(i).text(1,std::to_string(value));
+                    i++;
+                }
+                for(; i < 32 ; i++)
+                    reg_gui.at(i).text(1,"0");
+                inFile.close();
+            }
+        
+        path = "in/benchmarks/fatorial/memory.txt";
+        inFile.open(path);
+            if(!inFile.is_open())
+                show_message("Arquivo inválido","Não foi possível abrir o arquivo!");
+            else
+            {
+                int i = 0;
+                int value;
+                while(inFile >> value && i < 500)
+                {
+                    memory.Set(i,std::to_string(value));
+                    i++;
+                }
+                for(; i < 500 ; i++)
+                {
+                    memory.Set(i,"0");
+                }
+                inFile.close();
+            }
+
+    });
+
 
     vector<string> columns = {"#","Name","Busy","Op","Vj","Vk","Qj","Qk","A"}; 
     for(unsigned int i = 0 ; i < columns.size() ; i++)
